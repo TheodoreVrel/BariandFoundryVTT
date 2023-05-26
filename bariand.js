ff473a1a07dacd760b57b6d0186bb6ab6f44a4f7
Ted <41772561+TheodoreVrel@users.noreply.github.com>
2023-05-26 11:26:18 +0200

    Actor sheet works

--- bariand.js
+++ bariand.js
@@ -1,17 +1,17 @@
 // import { bariand } from "./modules/config.js";
-// import { BariandActorSheet } from "./modules/sheets/bariand-actor-sheet.js";
+import BariandActorSheet from "./modules/sheets/bariandActorSheet.js";
 import BariandItemSheet from "./modules/sheets/bariandItemSheet.js";
 
 Hooks.once("init", function () {
   console.log("bariand - initialisation");
 
-  // CONFIG.bariand = bariand;
+  //CONFIG.bariand = bariand;
 
-  // Actors.unregisterSheet("core", ActorSheet);
-  // Actors.registerSheet("bariand", BariandActorSheet, {
-  //   types: ["personnage"],
-  //   makeDefault: true,
-  // });
+  Actors.unregisterSheet("core", ActorSheet);
+  Actors.registerSheet("bariand", BariandActorSheet, {
+    types: ["1"],
+    makeDefault: true,
+  });
   Items.unregisterSheet("core", ItemSheet);
   Items.registerSheet("bariand", BariandItemSheet, { makeDefault: true });
 });
---
+++ modules/sheets/bariandActorSheet.js
@@ -0,0 +1,13 @@
+export default class BariandActorSheet extends ActorSheet {
+  get template() {
+    return `systems/bariand/templates/sheets/bariand-actor-sheet.html`;
+  }
+
+  // getData() {
+  //   const data = super.getData();
+
+  //   data.config = CONFIG.bariand;
+  //   console.log({ data });
+  //   return data;
+  // }
+}
